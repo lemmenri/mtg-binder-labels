a2003088c65556bb3bb560a5d736abf7463a769b
lemmenri <rick@joberi.nl>
2023-07-01 00:57:51 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,207 @@
-import Image from 'next/image'
+'use client'
+
+import * as sets from '../data/sets.json'
+
+const setType = "expansion"
+const setType2 = "core"
+const setType3 = "masters"
+const setType4 = "draft_innovation"
+const setType5 = "masterpiece"
+const setType6 = "starter"
+const setType7 = "from_the_vault"
+const setType8 = "funny"
+const skippedSets = [
+  "Summer Magic / Edgar",
+  "Innistrad: Midnight Hunt",
+  "Innistrad: Crimson Vow",
+  "Adventures in the Forgotten Realms",
+  "Kamigawa: Neon Dynasty",
+  "Streets of New Capenna",
+  "Dominaria United",
+  "The Brothers' War",
+  "Phyrexia: All Will Be One",
+  "March of the Machine",
+  "March of the Machine: The Aftermath",
+  "Chronicles Foreign Black Border",
+  "Renaissance",
+  "Rinascimento",
+  "Mystery Booster",
+  "Mystery Booster Retail Edition Foils",
+  "The List",
+  "Universes Within",
+  "Dominaria Remastered",
+  "Commander Masters",
+  "Jumpstart",
+  "Modern Horizons 1 Timeshifts",
+  "Innistrad: Double Feature",
+  "Commander Legends: Battle for Baldur's Gate",
+  "Jumpstart 2022",
+  "The Lord of the Rings: Tales of Middle-earth",
+  "Happy Holidays",
+  "2016 Heroes of the Realm",
+  "HasCon 2017",
+  "2017 Heroes of the Realm",
+  "2018 Heroes of the Realm",
+  "Ponies: The Galloping",
+  "Mystery Booster Playtest Cards 2019",
+  "2019 Heroes of the Realm",
+  "2020 Heroes of the Realm",
+  "Mystery Booster Playtest Cards 2021",
+  "2021 Heroes of the Realm",
+  "Unfinity Sticker Sheets",
+  "The List (Unfinity Foil Edition)",
+  "Transformers",
+  "Magic 2015 Clash Pack",
+  "Fate Reforged Clash Pack",
+  "Magic Origins Clash Pack",
+  "From the Vault: Dragons",
+  "From the Vault: Exiled",
+]
+let currentBinder = 0
+let setIndexInCurrentBinder = 1
+const setData: {
+  name: string,
+  image: string,
+  binder: number,
+}[] = []
+const binderData: {
+  binder: number,
+  fontSize: string,
+  svgSize: number,
+  sets: {
+    name: string,
+    image: string,
+  }[]
+}[] = []
+
+const binders = [
+  { binder: 1, nrOfSets: 9 }, // Aranbian Nights
+  { binder: 2, nrOfSets: 6 }, // Visions
+  { binder: 3, nrOfSets: 5 }, // Urza's Legacy
+  { binder: 4, nrOfSets: 6 }, // Invasion
+  { binder: 5, nrOfSets: 6 }, // Onslaught
+  { binder: 6, nrOfSets: 4 }, // Champions of Kamigawa
+  { binder: 7, nrOfSets: 4 }, // Guildpact
+  { binder: 8, nrOfSets: 6 }, // Time Spiral Timeshifted* == nog aanpassen
+  { binder: 9, nrOfSets: 6 }, // Eventide
+  { binder: 10, nrOfSets: 6 }, // Rise of the Eldrazi
+  { binder: 11, nrOfSets: 5 }, // Avacyn Restored
+  { binder: 12, nrOfSets: 4 }, // Born of the Gods
+  { binder: 13, nrOfSets: 3 }, // Dragons of Tarkir
+  { binder: 14, nrOfSets: 4 }, // Shadows over Innistrad
+  { binder: 15, nrOfSets: 4 }, // Amonkhet
+  { binder: 16, nrOfSets: 4 }, // Dominaria
+  { binder: 17, nrOfSets: 3 }, // Throne of Eldraine
+  { binder: 18, nrOfSets: 3 }, // Zendikar Rising
+  { binder: 19, nrOfSets: 8 }, // Limited Edition Alpha
+  { binder: 20, nrOfSets: 4 }, // Classic Sixth Edition
+  { binder: 21, nrOfSets: 3 }, // Tenth Edition
+  { binder: 22, nrOfSets: 4 }, // Magic 2012
+  { binder: 23, nrOfSets: 4 }, // Magic Origins
+  { binder: 24, nrOfSets: 5 },
+  { binder: 25, nrOfSets: 6 },
+  { binder: 26, nrOfSets: 6 },
+  { binder: 27, nrOfSets: 17 },
+  { binder: 28, nrOfSets: 20 },
+]
+
+const getSetData = (type: string) => {
+  sets.data.forEach(set => {
+    if (skippedSets.find(skippedSet => skippedSet === set.name)) { return }
+    if (set.set_type === type && set.digital === false) {
+      setData.unshift({
+        name: set.name,
+        image: set.icon_svg_uri,
+        binder: 0
+      })
+    }
+  })
+}
+
+getSetData(setType8)
+getSetData(setType7)
+getSetData(setType6)
+getSetData(setType5)
+getSetData(setType4)
+getSetData(setType3)
+getSetData(setType2)
+getSetData(setType)
+
+
+setData.forEach(set => {
+  set.binder = binders[currentBinder].binder
+  if (setIndexInCurrentBinder === binders[currentBinder].nrOfSets) {
+    // move to new binder
+    currentBinder++
+    setIndexInCurrentBinder = 1
+  } else {
+    // increment set counter for current binder
+    setIndexInCurrentBinder++
+  }
+})
+
+binders.forEach(binder => {
+  let fontSize = "text-base"
+  if (binder.nrOfSets >= 4) { fontSize = "text-sm" }
+  if (binder.nrOfSets >= 7) { fontSize = "text-xs" }
+  let svgSize = 120
+  if (binder.nrOfSets === 2) { svgSize = 90 }
+  if (binder.nrOfSets === 3) { svgSize = 75 }
+  if (binder.nrOfSets === 4) { svgSize = 54 }
+  if (binder.nrOfSets === 5) { svgSize = 40 }
+  if (binder.nrOfSets === 6) { svgSize = 30 }
+  if (binder.nrOfSets === 7) { svgSize = 28 }
+  if (binder.nrOfSets === 8) { svgSize = 21 }
+  if (binder.nrOfSets === 9) { svgSize = 20 }
+  if (binder.nrOfSets >= 10) { svgSize = 18 }
+  binderData.push({
+    binder: binder.binder,
+    fontSize: fontSize,
+    svgSize: svgSize,
+    sets: []
+  })
+})
+
+setData.forEach(set => {
+  binderData[set.binder - 1].sets.push({
+    name: set.name,
+    image: set.image
+  })
+})
+
+console.log(sets)
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div id="page" className="flex flex-wrap content-start">
+        {
+          binderData.map((binder) => {
+            return (
+              <div key={binder.binder} className='flex flex-col p-3 border bg-white w-[60mm] h-[95mm]'>
+                <div className='overflow-hidden'>
+                  {
+                    binder.sets.map((set) => {
+                      return (
+                        <div key={set.name} className={`${binder.sets.length >= 10 === true && "flex flex-row space-x-2 justify-between"} ${binder.fontSize}`}>
+                          <div className={`flex justify-center font-semibold ${binder.sets.length >= 10 === true ? "text-left" : "text-center"} ${binder.fontSize}`}>
+                            {`${set.name}`}
+                          </div>
+                          <div className='flex justify-center'>
+                            <svg width={binder.svgSize} height={binder.svgSize}>
+                              <image href={set.image} width={binder.svgSize} height={binder.svgSize} />
+                            </svg>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            )
+          })}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
